ec050e9d34ed643ac29be23822626ba8bd955b1a
Ajith Kumar R <ajithkumardotr@gmail.com>
2023-02-12 13:59:48 +0530

    Initial commit

---
+++ eCommerce/Buyer/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=50)
+    ph_no = models.CharField(max_length=10)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.user.first_name+" "+self.user.last_name)
+
+    class Meta:
+        verbose_name_plural = "Customer Details"
+
+    @property
+    def customer_name(self):
+        return "%s %s" % (self.user.first_name, self.user.last_name)
